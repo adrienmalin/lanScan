0d56163339de32142316476282c08f7b44dd0d91
adrien <adrien@malingrey.fr>
2024-10-07 20:33:03 +0200

    filter input

--- rdp.php
+++ rdp.php
@@ -1,6 +1,10 @@
 <?php
 
-header('Content-Disposition: attachment; filename='.str_replace(':', '_', $_GET['v']).'.rdp');
+$host = filter_input(INPUT_GET, 'v', FILTER_VALIDATE_DOMAIN) ?: filter_input(INPUT_GET, 'v', FILTER_VALIDATE_IP);
+if (!$host) {
+    exit();
+}
+
+header('Content-Disposition: attachment; filename='.str_replace(':', '_', $host).'.rdp');
 header('Content-Type: application/rdp');
-echo "full address:s:${_GET['v']}\n";
-exit();
+echo "full address:s:$host\n";
