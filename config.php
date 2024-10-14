2337fe1e601742cdd2a0a415712797b38865d91e
adrien <adrien@malingrey.fr>
2024-10-14 15:40:50 +0200

    little fixes

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $BASEDIR = "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}:{$_SERVER['SERVER_PORT']}" . dirname($_SERVER['REQUEST_URI']);
 
 $LANSCAN_OPTIONS = [
-    'PS'         => 'ssh,http,https,msrpc,microsoft-ds',
+    'PS'         => 'microsoft-ds',
     'F'          => true,
     'T5'         => true,
     'stylesheet' => "$BASEDIR/lanScan.xsl"
